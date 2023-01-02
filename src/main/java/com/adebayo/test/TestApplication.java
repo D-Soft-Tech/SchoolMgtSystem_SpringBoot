fc3597e520577488e6716b6ba5e42fe7ba6d0bf4
DSoftTech <81322397+DSoftTech@users.noreply.github.com>
2023-01-02 22:58:13 +0100

    Removed unused imports and classes

--- src/main/java/com/adebayo/test/TestApplication.java
+++ src/main/java/com/adebayo/test/TestApplication.java
@@ -1,15 +1,7 @@
 package com.adebayo.test;
 
-import com.adebayo.test.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class TestApplication {
